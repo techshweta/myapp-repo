e38cf7dcba13ad587501b0445347b86243392861
shweta <shwetakumari094@gmail.com>
2025-09-28 01:31:14 +0530

    updated file 14

--- Jenkinsfile
+++ Jenkinsfile
@@ -84,12 +84,12 @@ pipeline {
                      sh """
                         ssh -o BatchMode=yes ubuntu@${ec2_ip} "
                         if ! command -v docker &> /dev/null; then
-                        sudo apt-get update -y
-                        sudo apt-get install -y docker.io
-                        sudo systemctl enable docker
-                        sudo systemctl start docker
-                        fi
-
+                             sudo apt-get update -y
+                             sudo apt-get install -y docker.io
+                             sudo usermod -aG docker ubuntu
+                             sudo systemctl enable docker
+                             sudo systemctl start docker
+                        fi   
                         sudo docker stop myapp || true
                         sudo docker rm -f myapp || true
                         sudo docker pull ${IMAGE_NAME}:latest
--- terraform/main.tf
+++ terraform/main.tf
@@ -143,15 +143,6 @@ resource "aws_instance" "app" {
               sudo apt install -y openjdk-11-jdk tomcat9 tomcat9-admin
               sudo systemctl enable tomcat9
               sudo systemctl start tomcat9
-              sudo apt-get install -y ca-certificates curl gnupg lsb-release
-              sudo mkdir -p /etc/apt/keyrings
-              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-              echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-              sudo apt-get update -y
-              sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-              sudo usermod -aG docker ubuntu
-              sudo systemctl enable docker
-              sudo systemctl start docker
               EOF
 }
 
